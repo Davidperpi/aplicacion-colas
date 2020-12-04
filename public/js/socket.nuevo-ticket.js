429761e93ed2766ca6117c82828c02d9cf019580
David <davidperpi7@gmail.com>
2020-12-04 19:31:56 +0100

    Primera subida

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,22 @@
+// Comando para establecer la conexión 
+let socket = io();
+
+let label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Desconectado del servidor');
+});
+
+socket.on('estadoActual', function(ticket) {
+    label.text(ticket.actual);
+});
+
+$('button').on('click', function() {
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+        label.text(siguienteTicket);
+    });
+});
